f9966717dc7b2e764ac09b26db832564e6b79c2b
multiverse2011 <multiverse2011@gmail.com>
2018-05-18 19:20:35 +0900

    とりあえず両モータ対応

--- ER2-esp32.ino
+++ ER2-esp32.ino
@@ -1,10 +1,14 @@
 /* 使うピンの定義 */
 const int IN1 = 25;
 const int IN2 = 26;
+const int IN3 = 32;
+const int IN4 = 33;
 
 /* チャンネルの定義 */
 const int CHANNEL_0 = 0;
 const int CHANNEL_1 = 1;
+const int CHANNEL_2 = 2;
+const int CHANNEL_3 = 3;
 
 const int LEDC_TIMER_BIT = 8;   // PWMの範囲(8bitなら0〜255、10bitなら0〜1023)
 const int LEDC_BASE_FREQ = 490; // 周波数(Hz)
@@ -13,16 +17,20 @@ const int VALUE_MAX = 255;      // PWMの最大値
 void setup() {
   pinMode(IN1, OUTPUT); // IN1
   pinMode(IN2, OUTPUT); // IN2
+  pinMode(IN3, OUTPUT); // IN3
+  pinMode(IN4, OUTPUT); // IN4
 
   // ピンのセットアップ
   ledcSetup(CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
   ledcSetup(CHANNEL_1, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
+  ledcSetup(CHANNEL_2, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
+  ledcSetup(CHANNEL_3, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
 
   // ピンのチャンネルをセット
   ledcAttachPin(IN1, CHANNEL_0);
   ledcAttachPin(IN2, CHANNEL_1);
-
-  Serial.begin(38400);
+  ledcAttachPin(IN3, CHANNEL_2);
+  ledcAttachPin(IN4, CHANNEL_3);
 }
 
 void loop() {
@@ -34,7 +42,6 @@ void loop() {
   delay(3000);
   coast();      // 空転
   delay(3000);
-  Serial.print("Hello");
 }
 
 // 正転
@@ -44,6 +51,8 @@ void forward(uint32_t pwm) {
   }
   ledcWrite(CHANNEL_0, pwm);
   ledcWrite(CHANNEL_1, 0);
+  ledcWrite(CHANNEL_2, pwm);
+  ledcWrite(CHANNEL_3, 0);
 }
 
 // 逆転
@@ -53,17 +62,23 @@ void reverse(uint32_t pwm) {
   }
   ledcWrite(CHANNEL_0, 0);
   ledcWrite(CHANNEL_1, pwm);
+  ledcWrite(CHANNEL_2, 0);
+  ledcWrite(CHANNEL_3, pwm);
 }
 
 // ブレーキ
 void brake() {
   ledcWrite(CHANNEL_0, VALUE_MAX);
   ledcWrite(CHANNEL_1, VALUE_MAX);
+  ledcWrite(CHANNEL_2, VALUE_MAX);
+  ledcWrite(CHANNEL_3, VALUE_MAX);
 }
 
 // 空転
 void coast() {
   ledcWrite(CHANNEL_0, 0);
   ledcWrite(CHANNEL_1, 0);
+  ledcWrite(CHANNEL_2, 0);
+  ledcWrite(CHANNEL_3, 0);
 }
 
