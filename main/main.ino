4f743b131f1c01474218622a0b5e50a013650ce1
Dai1678 <dai7774@gmail.com>
2018-10-08 04:09:43 +0900

    ifブロック内の取得完了

--- main/main.ino
+++ main/main.ino
@@ -40,10 +40,10 @@ void setup()
 }
 
 void loop()
-{  
-    float distance = sensor.getDistance();
-    Serial.print("距離:    ");
-    Serial.println(distance);
+{
+    // float distance = sensor.getDistance();
+    // Serial.print("距離:    ");
+    // Serial.println(distance);
     udp.recieve_packet();
 
     String commands = udp.get_packet_buffer();
@@ -58,9 +58,12 @@ void loop()
         int loop_count = 0;               //ループ回数
         int j = 0;                        //loop_models配列の要素数
 
+        BlockModel if_models[100];      //ifブロックで囲まれた範囲内のBlockModelが入っている配列
+        boolean is_in_if_block = false; //if対象のブロックかどうか
+        int m = 0;                      //if_models配列の要素数
+
         for (int i = 0; i < 100; i++)
         {
-            Serial.println(models[i].get_block_state());
             if (models[i].get_block_state() == 0)
             {
                 break;
@@ -73,6 +76,43 @@ void loop()
                 Serial.println(loop_count);
                 is_in_loop_block = true;
             }
+            else if (models[i].get_block_state() == 7)
+            {
+                Serial.println("ifスタート");
+                is_in_if_block = true;
+
+                Serial.println(models[i].get_block_state());
+                Serial.println(models[i].get_left_speed());
+                Serial.println(models[i].get_right_speed());
+                Serial.println(models[i].get_time());
+
+                if_models[m].set_block_state(models[i].get_block_state());
+                if_models[m].set_left_speed(models[i].get_left_speed());
+                if_models[m].set_right_speed(models[i].get_right_speed());
+                if_models[m].set_time(models[i].get_time());
+                m++;
+            }
+
+            else if (models[i].get_block_state() == 8)
+            {
+                Serial.println("ifエンド");
+
+                Serial.println(models[i].get_block_state());
+                Serial.println(models[i].get_left_speed());
+                Serial.println(models[i].get_right_speed());
+                Serial.println(models[i].get_time());
+
+                if_models[m].set_block_state(models[i].get_block_state());
+                if_models[m].set_left_speed(models[i].get_left_speed());
+                if_models[m].set_right_speed(models[i].get_right_speed());
+                if_models[m].set_time(models[i].get_time());
+
+                //TODO if_models をif解釈関数に投げて、run_motorに投げる
+
+                is_in_if_block = false;
+                m = 0;
+            }
+
             else if (models[i].get_block_state() == 6) //ループエンドのブロックのとき、ループ対象のブロックをループ回数分処理
             {
                 Serial.println("ループエンド");
@@ -98,11 +138,33 @@ void loop()
                 loop_count = 0;
                 j = 0;
             }
+            else if (is_in_if_block)
+            {
+                Serial.print("if: state:   ");
+                Serial.println(models[i].get_block_state());
+                Serial.print("if: left_speed:   ");
+                Serial.println(models[i].get_left_speed());
+                Serial.print("if: right_speed:   ");
+                Serial.println(models[i].get_right_speed());
+                Serial.print("if: time:   ");
+                Serial.println(models[i].get_time());
+
+                if_models[m].set_block_state(models[i].get_block_state());
+                if_models[m].set_left_speed(models[i].get_left_speed());
+                if_models[m].set_right_speed(models[i].get_right_speed());
+                if_models[m].set_time(models[i].get_time());
+                m++;
+            }
+
             else if (is_in_loop_block) //ループ対象のブロックをloop_models配列に入れる
             {
+                Serial.print("loop: state:   ");
                 Serial.println(models[i].get_block_state());
+                Serial.print("loop: left_speed:   ");
                 Serial.println(models[i].get_left_speed());
+                Serial.print("loop: right_speed:   ");
                 Serial.println(models[i].get_right_speed());
+                Serial.print("loop: time:   ");
                 Serial.println(models[i].get_time());
 
                 loop_models[j].set_block_state(models[i].get_block_state());
