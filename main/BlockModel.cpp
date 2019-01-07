80753ad34d980634fe98eee6d039d4bb332c25bd
yuusuke <yuusukeogawara@gmail.com>
2019-01-08 01:00:20 +0900

    formatを変更

--- main/BlockModel.cpp
+++ main/BlockModel.cpp
@@ -1,84 +1,49 @@
-#include "Arduino.h"
 #include "BlockModel.h"
+#include "Arduino.h"
 
-BlockModel::BlockModel(){}
-BlockModel::BlockModel(int block_state, int left_speed, int right_speed, int time)
-{
-    set_block_state(block_state);
-    set_right_speed(right_speed);
-    set_left_speed(left_speed);
-    set_time(time);
+BlockModel::BlockModel() {}
+BlockModel::BlockModel(int block_state, int left_speed, int right_speed,
+                       int time) {
+  set_block_state(block_state);
+  set_right_speed(right_speed);
+  set_left_speed(left_speed);
+  set_time(time);
 }
 
-int BlockModel::get_block_state()
-{
-    return this->block_state;
+int BlockModel::get_block_state() { return this->block_state; }
+
+void BlockModel::set_block_state(int block_state) {
+  this->block_state = block_state;
 }
 
-void BlockModel::set_block_state(int block_state)
-{
-    this->block_state = block_state;
+int BlockModel::get_right_speed() { return this->right_speed; }
+
+void BlockModel::set_right_speed(int right_speed) {
+  this->right_speed = right_speed;
 }
 
-int BlockModel::get_right_speed()
-{
-    return this->right_speed;
+int BlockModel::get_left_speed() { return this->left_speed; }
+
+void BlockModel::set_left_speed(int left_speed) {
+  this->left_speed = left_speed;
 }
 
-void BlockModel::set_right_speed(int right_speed)
-{
-    this->right_speed = right_speed;
-}
+int BlockModel::get_time() { return this->time; }
 
-int BlockModel::get_left_speed()
-{
-    return this->left_speed;
-}
+void BlockModel::set_time(int time) { this->time = time; }
 
-void BlockModel::set_left_speed(int left_speed)
-{
-    this->left_speed = left_speed;
-}
+int BlockModel::get_loop_count() { return this->time; }
 
-int BlockModel::get_time()
-{
-    return this->time;
-}
+void BlockModel::set_loop_count(int loop_count) { this->time = loop_count; }
 
-void BlockModel::set_time(int time)
-{
-    this->time = time;
-}
+int BlockModel::get_if_threshold() { return this->time; }
 
-int BlockModel::get_loop_count()
-{
-    return this->time;
-}
+void BlockModel::set_if_threshold(int sensor_num) { this->time = sensor_num; }
 
-void BlockModel::set_loop_count(int loop_count)
-{
-    this->time = loop_count;
-}
-
-int BlockModel::get_if_threshold()
-{
-    return this->time;
-}
-
-void BlockModel::set_if_threshold(int sensor_num)
-{
-    this->time = sensor_num;
-}
-
-
-int BlockModel::get_if_comparison()
-{
-    return this->left_speed;
-}
+int BlockModel::get_if_comparison() { return this->left_speed; }
 // comparisonの値は1 or 2
 // 1は >
 // 2は <
-void BlockModel::set_if_comparison(int comparison)
-{
-    this->left_speed = comparison;
+void BlockModel::set_if_comparison(int comparison) {
+  this->left_speed = comparison;
 }
--- main/Udp.cpp
+++ main/Udp.cpp
@@ -1,12 +1,12 @@
 #include "Udp.h"
-#include "Arduino.h"
 #include <WiFi.h>
 #include <WiFiUdp.h>
+#include "Arduino.h"
 
-Udp::Udp(){}
+Udp::Udp() {}
 
-void Udp::setup_udp(char ssid[], char password[], IPAddress local_ip, IPAddress gateway, IPAddress subnet)
-{
+void Udp::setup_udp(char ssid[], char password[], IPAddress local_ip,
+                    IPAddress gateway, IPAddress subnet) {
   WiFi.config(local_ip, gateway, subnet);
   delay(100);
 
@@ -14,8 +14,7 @@ void Udp::setup_udp(char ssid[], char password[], IPAddress local_ip, IPAddress 
   delay(100);
 
   Serial.print("WiFi connecting");
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(100);
   }
@@ -23,18 +22,16 @@ void Udp::setup_udp(char ssid[], char password[], IPAddress local_ip, IPAddress 
   Serial.println(WiFi.localIP());
 
   Serial.println("Starting UDP");
-  wifi_udp.begin(local_port); // UDP通信の開始(引数はポート番号)
+  wifi_udp.begin(local_port);  // UDP通信の開始(引数はポート番号)
   Serial.print("Local port: ");
   Serial.println(local_port);
 }
 
-void Udp::recieve_packet()
-{
+void Udp::recieve_packet() {
   int packet_size = wifi_udp.parsePacket();
-  
-  if (packet_size)
-  {
-    //UDP情報の表示
+
+  if (packet_size) {
+    // UDP情報の表示
     Serial.print("Received packet of size ");
     Serial.println(packet_size);
 
@@ -45,30 +42,23 @@ void Udp::recieve_packet()
   }
 }
 
-String Udp::get_packet_buffer()
-{
+String Udp::get_packet_buffer() {
   String packetStr = String(packet_buffer);
   return packetStr;
 }
 
-void Udp::clear_packet_buffer()
-{
+void Udp::clear_packet_buffer() {
   int packetBuffer_length = strlen(packet_buffer);
-  memset(packet_buffer ,'\0',packetBuffer_length);
+  memset(packet_buffer, '\0', packetBuffer_length);
 }
 
-void Udp::send_data(IPAddress remote_ip, char text[])
-{ 
+void Udp::send_data(IPAddress remote_ip, char text[]) {
   Serial.println("send...");
-  
+
   wifi_udp.beginPacket(remote_ip, local_port);
   wifi_udp.printf(text);
   wifi_udp.endPacket();
 }
-void Udp::disconnect() {
-  WiFi.disconnect();
-}
+void Udp::disconnect() { WiFi.disconnect(); }
 
-IPAddress Udp::get_remote_ip() {
-  return wifi_udp.remoteIP();
-}
+IPAddress Udp::get_remote_ip() { return wifi_udp.remoteIP(); }
